aca79a0b56425f2ae0f60a2bae8ee3fbcb6dd9ce
marcosismayle <marcosismayle@gmail.com>
2023-01-15 01:09:02 -0300

    last updated

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,6 @@ let year = d.getFullYear();
 
 document.getElementById("currentYear").innerHTML = year;
 
-let currentDate = new Date();
-let year2 = currentDate.getFullYear();
-let month = currentDate.getMonth() + 1;
-let date = currentDate.getDate();
-let hours = currentDate.getHours();
-let minutes = currentDate.getMinutes();
-let seconds = currentDate.getSeconds();
+let lastmodified = new Date(document.lastModified);
 
-let dateString = "Last Updated: " + month + "/" + date + "/" + year2 + " " + hours + ":" + minutes + ":" + seconds;
-
-document.getElementById("lastUpdated").innerHTML = dateString;
+document.getElementById("lastUpdated").innerHTML = lastmodified
